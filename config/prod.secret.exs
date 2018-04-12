8aa560db53be7f0fdef0901def7b888661433719
zeucxb <zeucxb@gmail.com>
2018-04-11 21:57:16 -0300

    Heroku ready

---
+++ config/prod.secret.exs
@@ -0,0 +1,19 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you'll likely want to automate and keep away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or yourself later on).
+config :scanbu_api, DesejoPuroWeb.Endpoint,
+  secret_key_base: Map.fetch(System.get_env(), "SECRET_KEY_BASE")
+
+# Configure your database
+config :scanbu_api, DesejoPuro.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
