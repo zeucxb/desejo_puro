422ff2288c1102d3678f65a0886ad33f770ef0f5
zeucxb <zeucxb@gmail.com>
2018-04-11 23:23:15 -0300

    Correct SECRET_KEY_BASE

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -8,8 +8,7 @@ use Mix.Config
 # file or create a script for recreating it, since it's
 # kept out of version control and might be hard to recover
 # or recreate for your teammates (or yourself later on).
-config :desejo_puro, DesejoPuroWeb.Endpoint,
-  secret_key_base: Map.fetch(System.get_env(), "SECRET_KEY_BASE")
+config :desejo_puro, DesejoPuroWeb.Endpoint, secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
 config :desejo_puro, DesejoPuro.Repo,
